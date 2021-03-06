bfffe79614e91301e55a3ab98c8815b26768eb07
songgoeun <35678491+songgoeun@users.noreply.github.com>
2021-03-06 21:23:57 +0900

    import Ace

--- database.js
+++ database.js
@@ -2,7 +2,6 @@ module.exports = {
 	host     : 'localhost',
 	user     : 'root',
 	password: '1863rndnjs9158!',
-	password: 'qusgPwlsa',
 	database: 'bg',
 	multipleStatements: true,
 	dateStrings: 'date'
