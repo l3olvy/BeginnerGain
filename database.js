a10109db5d9d88e498ec39edb8c937e91e2f936b
Bon Kim <qhsl0615@naver.com>
2024-01-19 22:55:13 +0900

    [add] database

--- database.js
+++ database.js
@@ -1,8 +1,12 @@
 module.exports = {
-	host     : 'localhost',
-	user     : 'root',
-	password: 'Qlxnql-?1021',
-	database: 'bg',
+	host : 'ep-proud-shadow-84038243.eu-central-1.pg.koyeb.app',
+	user : 'koyeb-adm',
+	password : 'Y1Mntij0ASPk',
+	database : 'bg',
+	// host     : 'localhost',
+	// user     : 'root',
+	// password: 'Qlxnql-?1021',
+	// database: 'bg',
 	multipleStatements: true,
 	dateStrings: 'date'
 };
